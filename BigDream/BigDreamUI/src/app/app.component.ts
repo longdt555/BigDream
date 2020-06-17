d72e56c1b4cbb644c30aee2803abd431d687fdf4
longdt <longdt555@gmail.com>
2020-06-17 17:24:15 +0700

    Update stucture

--- BigDream/BigDreamUI/src/app/app.component.ts
+++ BigDream/BigDreamUI/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewChild, Renderer2, Inject, ElementRef, NgModule } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { Router, NavigationEnd } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 import { map, filter, scan } from 'rxjs/operators';
 import {Location} from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,42 +14,42 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class AppComponent implements OnInit {
-  private _router: Subscription;
-  @ViewChild(NavbarComponent) navbar: NavbarComponent;
+  // private _router: Subscription;
+  // @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
   constructor(private renderer: Renderer2, private router: Router, @Inject(DOCUMENT) private document: any, private element: ElementRef, public location: Location) { }
   ngOnInit() {
-    var navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
-    this._router = this.router.events.pipe(filter((event => event instanceof NavigationEnd))).subscribe((event: NavigationEnd) => {
-      if (window.outerWidth > 991) {
-        window.document.children[0].scrollTop = 0;
-      } else {
-        window.document.activeElement.scrollTop = 0;
-      }
-      this.navbar.sidebarClose();
-    });
-    this.renderer.listen('window', 'scroll', (event) => {
-      const number = window.scrollY;
-      if (number > 150 || window.pageYOffset > 150) {
-        // add logic
-        navbar.classList.remove('navbar-transparent');
-      } else {
-        // remove logic
-        navbar.classList.add('navbar-transparent');
-      }
-    });
-    var ua = window.navigator.userAgent;
-    var trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-      // IE 11 => return version number
-      var rv = ua.indexOf('rv:');
-      var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-    if (version) {
-      var body = document.getElementsByTagName('body')[0];
-      body.classList.add('ie-background');
+    // var navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
+    // this._router = this.router.events.pipe(filter((event => event instanceof NavigationEnd))).subscribe((event: NavigationEnd) => {
+    //   if (window.outerWidth > 991) {
+    //     window.document.children[0].scrollTop = 0;
+    //   } else {
+    //     window.document.activeElement.scrollTop = 0;
+    //   }
+    //   this.navbar.sidebarClose();
+    // });
+    // this.renderer.listen('window', 'scroll', (event) => {
+    //   const number = window.scrollY;
+    //   if (number > 150 || window.pageYOffset > 150) {
+    //     // add logic
+    //     navbar.classList.remove('navbar-transparent');
+    //   } else {
+    //     // remove logic
+    //     navbar.classList.add('navbar-transparent');
+    //   }
+    // });
+    // var ua = window.navigator.userAgent;
+    // var trident = ua.indexOf('Trident/');
+    // if (trident > 0) {
+    //   // IE 11 => return version number
+    //   var rv = ua.indexOf('rv:');
+    //   var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    // }
+    // if (version) {
+    //   var body = document.getElementsByTagName('body')[0];
+    //   body.classList.add('ie-background');
 
-    }
+    // }
 
   }
 }
